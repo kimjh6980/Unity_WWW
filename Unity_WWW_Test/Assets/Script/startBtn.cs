2e076625d46107e57a50f3bf27f9d7ba1b748a96
kimjh6980 <kimjh6980@naver.com>
2018-03-07 15:08:36 +0900

    Logging input id

---
+++ Unity_WWW_Test/Assets/Script/startBtn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class startBtn : MonoBehaviour {
+
+    public Text InputEmailText;
+
+	// Use this for initialization
+	void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void startGame()
+    {
+        string ID = InputEmailText.text;
+        Debug.Log("ID = " + ID);
+    }
+}
