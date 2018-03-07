3a7e4cd3f3404783e5e6adafe8b9eadd269b9ff6
kimjh6980 <kimjh6980@naver.com>
2018-03-07 15:26:45 +0900

    Server Conn Success (now Server = QI_Teama)

--- Unity_WWW_Test/Assets/Script/startBtn.cs
+++ Unity_WWW_Test/Assets/Script/startBtn.cs
@@ -5,21 +5,56 @@ using UnityEngine.UI;
 
 public class startBtn : MonoBehaviour {
 
+    private string LoginUrl;
+
+    private string ID;
     public Text InputEmailText;
 
 	// Use this for initialization
 	void Start () {
-       
+        LoginUrl = "http://teama-iot.calit2.net/signinapp";
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
     public void startGame()
     {
-        string ID = InputEmailText.text;
+        ID = InputEmailText.text;
         Debug.Log("ID = " + ID);
+
+        StartCoroutine(LoginCo());
+    }
+
+    IEnumerator LoginCo()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("who", "0");
+        form.AddField("useremail", ID);
+        form.AddField("userpassword", "hiella123");
+
+        WWW webRequest = new WWW(LoginUrl, form);
+
+        StartCoroutine(WaitForRequest(webRequest));
+
+        Debug.Log(webRequest.text);
+        yield return webRequest;
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        yield return www;
+        if (www.error == null)
+        {
+            // request completed!
+            Debug.Log(www.text);
+        }
+        else
+        {
+            // something wrong!
+            Debug.Log("WWW error: " + www.error);
+        }
     }
 }
